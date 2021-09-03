fcbf9f56d73a48951b6ea7244fed31f20ab0db76
Mr_Denno <dennisturco@gmail.com>
2021-09-03 17:54:26 +0200

    first commit

---
+++ src/project/RandomPasswordGenerator.java
@@ -0,0 +1,22 @@
+package project;
+import java.awt.*;
+import javax.swing.*;
+
+import java.util.Random; //per il random number generator
+
+class RandomPasswordGenerator extends JFrame{
+	
+	public RandomPasswordGenerator() {
+		setTitle("Random Password Generator");
+		setSize(500, 400);
+		setLocation(700, 300);
+		setLayout(new BorderLayout());
+		setResizable(false);
+		getContentPane().setBackground(new Color(123, 50, 250)); //setta il colore dello sfondo
+		
+		
+		JPanel spunta1 = new JPanel("Lettere Maiuscole");
+		
+		JPanel spunta2 = new JPanel("Lettere Speciali")
+	}
+}
---
+++ src/project/RandomPasswordGeneratorMain.java
@@ -0,0 +1,13 @@
+package project;
+import javax.swing.JFrame;
+
+class RandomPasswordGeneratorMain {
+	
+	public static void main(String[] args) {
+		RandomPasswordGenerator nb = new RandomPasswordGenerator();		
+		nb.setVisible(true);
+		nb.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+}
+
